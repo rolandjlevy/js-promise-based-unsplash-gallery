60ee8f13a13684ec762e550d7264292db25014ce
Roland Levy <rolandjlevy@gmail.com>
2021-02-28 21:57:52 +0000

    Increased maxImages

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 let counter = 1;
-const maxImages = 15;
+const maxImages = 25;
 const baseUrl = 'https://source.unsplash.com/random';
 import UnsplashImage from './UnsplashImage.js';
 
