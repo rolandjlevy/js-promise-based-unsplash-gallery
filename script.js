b412d0aff19aedff0a647d16bf8265ab89990003
Roland Levy <rolandjlevy@gmail.com>
2021-02-28 19:49:40 +0000

    Refactored

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-function addImage(n) {
+const gallery = document.querySelector('.gallery');
+let counter = 1;
+const maxImages = 15;
+const baseUrl = 'https://source.unsplash.com/random';
+
+const addImage = (n) => {
   const li = document.createElement('li');
   const img = document.createElement('img');
-  img.src = `https://source.unsplash.com/random?${n}`
+  img.src = `${baseUrl}?${n}`;
   li.appendChild(img)
   return li;
 }
 
-const gallery = document.querySelector('.gallery');
-
-let counter = 1;
-
-while (counter++ < 15) {
+while (counter++ <= maxImages) {
   const img = addImage(counter);
   gallery.appendChild(img);
 }
