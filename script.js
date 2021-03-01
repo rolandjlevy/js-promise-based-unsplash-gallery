0a14cea5289e2df072e07e9999fffd3e772af0f5
Roland Levy <rolandjlevy@gmail.com>
2021-03-01 21:46:47 +0000

    Tried to get promise.all to work

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const maxImages = 25;
 const baseUrl = 'https://source.unsplash.com/random';
 import UnsplashImage from './UnsplashImage.js';
 
+const promises = [];
 while (counter++ <= maxImages) {
-  new UnsplashImage(container, baseUrl, counter);
+  const unsplashImage = new UnsplashImage(container, baseUrl, counter);
+  promises.push(unsplashImage.loadImage);
 }
+
+Promise.all(promises).then(values => {
+  values.forEach(promise => {
+    console.log({promise});
+  });
+});
